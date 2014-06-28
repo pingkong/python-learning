feec6f7665b5a30c9109df8162cd8dd2cccf8ef2
Luke Austen <l.austen@corp.badoo.com>
2014-06-28 21:32:23 +0100

    lesson 3 breaks computer

---
+++ lesson3.py
@@ -0,0 +1,38 @@
+import math
+
+TESTNUM=600851475143
+
+testrange=range(1,TESTNUM)
+
+#if TESTNUM % 2==0:
+#	print TESTNUM % 2, 1
+#else:
+#	print TESTNUM % 2, 2
+	
+#for i in testrange:
+#	testfact=TESTNUM/float(i) 
+#	testdiff=testfact-math.floor(TESTNUM/i)
+#	if testdiff==0:
+#		print testfact, "Is a factor"
+#		testrange2=range(2,integer(testfact))
+#		for j in testrange2:
+#	else:
+#		continue
+		
+for i in testrange:
+	if TESTNUM % i == 0: 
+		is_not_prime = 0
+		#print i, "Is a factor"
+		testrange2 = range(2,i-1)
+		for j in testrange2:
+			if i % j == 0:
+		#		print i, "is not prime"
+				is_not_prime =1
+			else:
+				continue	
+		if is_not_prime == 0:
+			print i, "is prime"
+		else:
+			continue
+	else:
+		continue
